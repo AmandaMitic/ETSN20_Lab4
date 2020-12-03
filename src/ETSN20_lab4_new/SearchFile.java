21ead2f94c4ea5c2773b1eef5e1ad9f4b7b16efd
AmandaMitic <37863733+AmandaMitic@users.noreply.github.com>
2020-12-03 21:22:27 +0100

    Added and implemented the final code and myTextFile.txt.

---
+++ src/ETSN20_lab4_new/SearchFile.java
@@ -0,0 +1,38 @@
+package ETSN20_lab4_new;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SearchFile {
+
+	public SearchFile() {}
+	
+public void printLines(String pattern, String fileName) throws FileNotFoundException{
+		
+//		Pattern pattern = new Pattern(fileName);
+		File file = new File (fileName);
+		Scanner readFile = new Scanner(file);
+		
+		while (readFile.hasNextLine()) {
+			String line = readFile.nextLine();		
+			
+			if(line.toLowerCase().indexOf(pattern.toLowerCase()) != -1) {
+				System.out.println(line);
+			}
+		}
+		readFile.close();
+		
+	}
+	
+	public static void main(String args[]) throws FileNotFoundException{
+		
+		SearchFile search = new SearchFile();
+		String pattern = args[1];
+		String fileName = args[2];
+		search.printLines(pattern, fileName);
+	
+	}
+
+}
+
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module ETSN20_lab4_new {
+}
